3c404488bd3f977aad09766228bef103232993e9
Júlia Teixeira <julia_vlj@hotmail.com>
2022-08-31 11:36:59 -0300

    finalizando a calculadora

--- Lista16.js
+++ Lista16.js
@@ -1,45 +1,61 @@
-/*6. Faça com que seu usuário escolha qual função ele deseja fazer
-com os números que ele inseriu, e depois de realizar essa
-operação pergunte se ele deseja fazer outra ou encerrar a
-aplicação.
-7. Crie um array para salvar o resultado das operações e crie
+/*7. Crie um array para salvar o resultado das operações e crie
 também uma função para exibir o histórico dos resultados.
 ATENÇÃO!! Faça um commit para cada item do exercício.*/
 
 var numA = 0
 var numB = 0
 var resultado = []
+var continuar = 1
+var contador = 0
 
 numA = parseFloat(prompt("Insira um número para A: "))
 numB = parseFloat(prompt("Insira outro número para B: "))
 
-var opcao = prompt("Qual opção você escolhe? 1 - soma / 2 - subtração / 3 - multiplacação / 4 - divisão ")
+while (continuar == 1) {
+    var opcao = prompt("Qual opção você escolhe? 1 - soma / 2 - subtração / 3 - multiplacação / 4 - divisão ")
 
-if (opcao == 1) {
-    console.log(Soma())
-}
-
-if (opcao == 2) {
-    Subtrair()
-}
-
-if (opcao == 3) {
-    function multiplicar() {
-        return numA * numB
+    if (opcao == 1) {
+        resultado[contador] = Soma()
+        console.log("O resultado da soma é: " + resultado[contador])
+        contador++
     }
-}
 
-if (opcao == 4) {
-    function dividir() {
-        return numA / numB
+    else if (opcao == 2) {
+        resultado[contador] = Subtrair()
+        console.log("O resultado da subtração é: " + resultado[contador])
+        contador++
     }
+
+    else if (opcao == 3) {
+        resultado[contador] = multiplicar()
+        console.log("O resultado da multiplicação é: " + resultado[contador])
+        contador++
+    }
+
+    else if (opcao == 4) {
+        resultado[contador] = dividir()
+        console.log("O resultado da divisão é: " + resultado[contador])
+        contador++
+    }
+    continuar = prompt("Deseja fazer outra operação ou deseja encerrar operação? 1 - sim / 2 - não")
+    ExibirHistorico()
 }
 
 function Soma() {
     return numA + numB
 }
+function Subtrair() {
+    return numA - numB
+}
 function multiplicar() {
     return numA * numB
+}
+function dividir() {
+    return numA / numB
+}
+function ExibirHistorico() {
+    for (var index = 0; index < contador; index++)
+        console.log(resultado[index])
 }
 
 
